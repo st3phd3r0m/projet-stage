f67a3598b1c1e0f815a99df5bd76e75527f70ef0
Stéphane Derom <stephane.derom@gmail.com>
2020-07-25 14:54:21 +0200

    Formulaire création/modification utilisateur

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -5,11 +5,17 @@ namespace App\Controller;
 use App\Entity\Users;
 use App\Form\UsersType;
 use App\Repository\UsersRepository;
+use App\Security\UserAuthenticator;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 /**
  * @Route("/admin/users")
@@ -34,11 +40,11 @@ class UsersController extends AbstractController
             'users' => $users
         ]);
     }
-    
+
     /**
      * @Route("/new", name="users_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
 
         $user = new Users;
@@ -47,17 +53,98 @@ class UsersController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-             $entityManager = $this->getDoctrine()->getManager();
-             $entityManager->persist($user);
-             $entityManager->flush();
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
 
-             return $this->redirectToRoute('users_index');
-         }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-         return $this->render('users/new.html.twig', [
-             'user' => $user,
-             'form' => $form->createView(),
-         ]);
+            //Envoi d'un message utilisateur
+            $this->addFlash('success', 'L\'utilisateur a bien été ajouté en base de données');
+
+            return $this->redirectToRoute('users_index');
+        }
+
+        return $this->render('users/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="users_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Users $user): Response
+    {
+        $form = $this->createForm(UsersType::class, $user);
+        $form->remove('plainPassword');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            //Envoi d'un message utilisateur
+            $this->addFlash('success', 'Les informations de l\'utilisateur ont bien été modifiées');
+
+            return $this->redirectToRoute('users_index');
+        }
+
+        return $this->render('users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/change/password", name="users_change_password", methods={"GET","POST"})
+     */
+    public function changePassword(Request $request, Users $user, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        if ($this->getUser()->getUserName() === $user->getUsername()) {
+
+            $form = $this->createForm(UsersType::class, $user);
+            $form->remove('email');
+            $form->remove('firstname');
+            $form->remove('lastname');
+            $form->remove('roles');
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // encode the plain password
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                $this->getDoctrine()->getManager()->flush();
+
+                //Envoi d'un message utilisateur
+                $this->addFlash('success', 'Votre mot de passe a bien été modifié');
+                return $this->redirectToRoute('users_index');
+
+            }else if ($form->isSubmitted() && !$form->isValid()) {
+                //Envoi d'un message utilisateur
+                $this->addFlash('fail', 'Formulaire incomplet ou invalide.');
+            }
+
+            return $this->render('users/edit.html.twig', [
+                'changepassword' => true,
+                'user' => $user,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        //Envoi d'un message utilisateur
+        $this->addFlash('fail', 'Vous devez être connecté en tant que '.$user->getFirstName().' '.$user->getLastName().' pour modifier le mot de passe associé à ce compte.');
+        return $this->redirectToRoute('users_index');
     }
 
     /**
@@ -72,32 +159,13 @@ class UsersController extends AbstractController
 
 
 
-    /**
-     * @Route("/{id}/edit", name="users_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Users $user): Response
-    {
-        $form = $this->createForm(UsersType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('users_index');
-        }
-
-        return $this->render('users/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
-    }
 
     /**
      * @Route("/{id}", name="users_delete", methods={"DELETE"})
      */
     public function delete(Request $request, Users $user): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($user);
             $entityManager->flush();
--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -6,10 +6,13 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UsersType extends AbstractType
@@ -29,22 +32,6 @@ class UsersType extends AbstractType
                     ])
                 ]
             ])
-            ->add('roles', ChoiceType::class, [
-                'required' => true,
-                'label' => 'Rôle utilisateur',
-                'expanded' => true,
-                'multiple' => true,
-                'choices' => [
-                    'Utilisateur' => 'ROLE_USER',
-                    'Administrateur' => 'ROLE_ADMIN'
-                ],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez choisir un rôle pour l\'utilisateur'
-                    ])
-                ]
-            ])
-            ->add('password')
             ->add('firstname', TextType::class, [
                 'required' => true,
                 'label' => 'Prénom',
@@ -62,6 +49,49 @@ class UsersType extends AbstractType
                         'message' => 'Veuillez saisir un nom'
                     ])
                 ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'label' => 'Inscrire en tant qu\'administrateur ?',
+                'expanded' => true,
+                'multiple' => true,
+                'choices' => [
+                    // 'Utilisateur' => 'ROLE_USER',
+                    'Oui' => 'ROLE_ADMIN'
+                ],
+                // 'constraints' => [
+                //     new NotBlank([
+                //         'message' => 'Veuillez choisir un rôle pour l\'utilisateur'
+                //     ])
+                // ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'required' => true,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes ne sont pas identiques !',
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'mapped' => false,
+                'options' => [
+                    'attr' =>[
+                         'class' => 'form-control form-control-lg form-control-a mb-3'
+                    ],
+                    'label_attr' => [
+                    'class' => 'policeForm'
+                ],
+                ],
+                
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez votre mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir minimum {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                    
+                ],
             ]);
     }
 
