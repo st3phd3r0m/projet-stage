f2fcd7b760a69791dfeb486aabd5db069c0a2bd6
Stéphane Derom <stephane.derom@gmail.com>
2020-07-27 16:00:36 +0200

    Formulaire des messages

--- src/Form/MessagesType.php
+++ src/Form/MessagesType.php
@@ -4,20 +4,75 @@ namespace App\Form;
 
 use App\Entity\Messages;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MessagesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('phone')
-            ->add('subject')
-            ->add('message')
-        ;
+            ->add('name', TextType::class, [
+                'required' => true,
+                'label' => 'Saisissez votre prénom et votre nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre prénom et votre nom'
+                    ])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Adresse e-mail',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une adresse e-mail'
+                    ]),
+                    new Email([
+                        'message' => 'Votre adresse e-mail n\'est pas valide'
+                    ])
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'required' => true,
+                'label' => 'Télephone',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un numéro de télephone'
+                    ])
+                ]
+            ])
+            ->add('subject', TextType::class, [
+                'required' => true,
+                'label' => 'Saisissez l\'objet de votre demande',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Champs recquis'
+                    ])
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'required' => true,
+                'label' => 'Saisissez votre message',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre commentaire',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => "Votre commentaire doit comporter au moins {{ limit }}
+                        caractères.",
+                    ])
+                ]
+            ]);
+        
     }
 
     public function configureOptions(OptionsResolver $resolver)
